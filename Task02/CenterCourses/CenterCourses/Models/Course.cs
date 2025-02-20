f8ce9458df03d810f415481c3f944bb8105ae0ed
ibriheem <73044321+ibriheem@users.noreply.github.com>
2025-02-20 22:29:17 +0200

    Task02

---
+++ Task02/CenterCourses/CenterCourses/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CenterCourses.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Degree { get; set; }
+        public int? MinDegree { get; set; }
+        public int? Hours { get; set; } 
+        public List<Instructor>? Instructors { get; set; } = new List<Instructor>();
+        public List<CrsResult>? CrsResult { get; set; } = new List<CrsResult>();
+    }
+}
---
+++ Task02/CenterCourses/CenterCourses/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CenterCourses.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Manager { get; set; }
+        public List<Instructor>? Instructors { get; set; } = new List<Instructor>();
+        public List<Trainee>? Trainees { get; set; } = new List<Trainee> { };
+    }
+}
