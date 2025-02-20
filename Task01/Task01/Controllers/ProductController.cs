60d81a9753297ce4b2efe702ecd8f794f07dc2a7
ibriheem <73044321+ibriheem@users.noreply.github.com>
2025-02-20 03:17:58 +0200

    Task01_MVC

---
+++ Task01/Task01/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using Task01.Models;
+
+namespace Task01.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductBL productBL = new ProductBL();
+        public IActionResult All()
+        {
+          //as model
+          List<Product> ProductsModel=productBL.GetAll();
+            //send data to view
+            return View("ShowAll",ProductsModel );//list of product
+        }
+
+        //Details
+        //endpoint /Product/Details/1 | /product/Details?id=1
+
+        public IActionResult Details(int id)
+        {
+            Product ProModel = productBL.GetById(id);
+            return View("Details", ProModel); // view Details , Model with type student
+        }
+
+    }
+}
---
+++ Task01/Task01/Models/ProductBL.cs
@@ -0,0 +1,26 @@
+﻿namespace Task01.Models
+{
+    public class ProductBL
+    {
+        public List<Product> Products;
+        public ProductBL()
+        {
+            Products = new List<Product>();
+            Products.Add(new Product() { Id = 1, Name = "Chesse", Price = 140, Description = "Normal", ImageUrl = "1.png" });
+            Products.Add(new Product() { Id = 2, Name = "Tea250gm", Price = 80, Description = "High", ImageUrl = "2.png" });
+            Products.Add(new Product() { Id = 3, Name = "Water Bottle", Price = 10, Description = "Normal", ImageUrl = "3.png" });
+            Products.Add(new Product() { Id = 4, Name = "Yogurt 100 gm", Price = 15, Description = "Normal", ImageUrl = "4.png" });
+            Products.Add(new Product() { Id = 5, Name = "Coffee 100 gm", Price = 80, Description = "High", ImageUrl = "5.png" });
+
+        }
+
+        public List<Product> GetAll()
+        {
+            return Products;
+        }
+        public Product GetById(int id)
+        {
+            return Products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
